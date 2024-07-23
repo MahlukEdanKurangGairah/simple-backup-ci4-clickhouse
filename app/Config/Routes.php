97f60340512f55c9f25f8039be937e295004e477
Jurig Culun <lailamajenun.1001@gmail.com>
2024-07-23 22:13:03 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -2,7 +2,3 @@
 use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 $routes->cli('/', 'Operasi_mysql::run');
-$routes->group('operasi-mysql',static function($routes){
-    $routes->cli('/', 'Operasi_mysql::run');
-    $routes->cli('backup-mysql-parquet/(:any)', 'Operasi_mysql::create_backup_mysql_parquet/$1');
-});
