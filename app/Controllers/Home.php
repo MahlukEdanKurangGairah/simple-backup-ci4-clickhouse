e0d4e068f433b04b89a5b16818bb8e9f4a54610e
Jurig Culun <lailamajenun.1001@gmail.com>
2024-07-23 22:14:05 +0700

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,42 +1,7 @@
 <?php
 namespace App\Controllers;
 class Home extends BaseController{
-    private $backup_dir;
-    public function __construct(){
-        helper([
-            'yaml','clickhouse','mysqlclient'
-        ]);
-        $this->my_model = new \App\Models\MyModel();
-    }
     public function index(){
-        /* $arTable = $this->my_model->showTables();
-        foreach ($arTable as $table) {
-            $table_name = $table[key($table)];
-            create_parquet($table_name);
-        }
-        restore_mysql('20240715');
-        return view('welcome_message',[
-            'ar_file'=>'',
-        ]); */
-    }
-    public function run(){
-        CLI::clearScreen();
-        $pilihan = CLI::promptByKey('Masukkan angka pilihan anda dan tekan enter (default: x)', [
-            'x'=>'Exit',
-            'b'=>'Backup Database',
-            'r'=>'Restore Database',
-        ]);
-        
-        if(strtolower(trim($pilihan))=='x'){
-            CLI::write('Exit');
-            exit;
-        }elseif(strtolower(trim($pilihan))=='b'){
-            $nama_folder = CLI::prompt('Masukan nama folder tempat backup data: ',date('Ymd'));
-            $this->create_backup_mysql_parquet($nama_folder);
-        }
-    }
-    public function backup_list(){}
-    public function create_backup(){
-        $folder = date('Ymd_Hi');
+        return view('welcome_message');
     }
 }
