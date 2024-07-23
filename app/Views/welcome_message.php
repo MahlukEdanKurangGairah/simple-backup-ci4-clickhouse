1ce7cc252c7a292d795430104c999287b3cea314
Jurig Culun <lailamajenun.1001@gmail.com>
2024-07-23 22:15:10 +0700

    Update welcome_message.php

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -8,14 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-<form action="http://172.20.10.5:8123?" id="form_query" method="post">
-    <input type="hidden" name="user" value="default">
-    <div class="row">
-        <div class="col-12"><textarea name="query" id="" cols="30" rows="10"></textarea></div>
-        <div class="col-12"><button class="btn btn-primary">Submit</button></div>
-    </div>
-</form>
-<div id="hasil"></div>
+<div id="hasil">SENGAJA KOSONG</div>
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/jquery-form@4.3.0/dist/jquery.form.min.js"></script>
